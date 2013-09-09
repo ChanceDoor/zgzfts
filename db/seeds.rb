6f14ba2c111a4e3482006e865de81c54f92c29ee
ChengDuo <cd19900419@hotmail.com>
2013-09-10 02:07:34 +0800

    重构new edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Admin.create(email: 'admin@zgzfts.com', password:'111222')
+Admin.create(email: 'admin@zgzfts.com', password:'11112222')
 News.create(title:'请添加新闻',body:'这是测试新闻',posttime:Time.now)
 Notice.create(title:'请添加公告',body:'这是测试公告',posttime:Time.now)
 Distribution.create(title:'请添加经销商信息',body:'这是测试信息',posttime:Time.now)

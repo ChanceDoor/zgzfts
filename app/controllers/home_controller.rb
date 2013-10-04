4d2581f3be4c63f3788482a6bd8297949b29c11e
ChengDuo <cd19900419@hotmail.com>
2013-10-04 15:40:06 +0800

    fix per_page test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @news = News.page(params[:news_page]).per(1)
+    @news = News.page(params[:news_page]).per(10)
     @notices = Notice.page(params[:notices_page]).per(10)
     @distributions = Distribution.page(params[:dis_page]).per(10)
     @books = Book.all

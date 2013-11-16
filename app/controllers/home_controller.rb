8c8a59319ae297c31338426f14adeebba15cbc0a
ChengDuo <cd19900419@hotmail.com>
2013-11-16 17:47:26 +0800

    add column

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,10 @@
 class HomeController < ApplicationController
   def index
     @news = News.page(params[:news_page]).per(10)
-    @notices = Notice.page(params[:notices_page]).per(10)
+    @notices = Notice.page(params[:notices_page]).per(5)
     @distributions = Distribution.page(params[:dis_page]).per(10)
     @books = Book.all
+    @posts = Post.page(params[:posts_page]).per(3)
+    @editor = Editor.find(@posts.first.editor_id)
   end
 end

c3fc28a24bd079fd363d979da56cd7ba817ce04e
ChengDuo <cd19900419@hotmail.com>
2014-04-08 19:11:31 +0800

    add taobao

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -46,6 +46,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:body, :cover, :posttime, :title, :price)
+    params.require(:book).permit(:body, :cover, :posttime, :title, :price, :url)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,114 +9,115 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131110094114) do
+ActiveRecord::Schema.define(version: 20140408105345) do
 
-  create_table "admins", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "admins", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "name"
   end
 
-  add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
-  add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
 
-  create_table "books", :force => true do |t|
+  create_table "books", force: true do |t|
     t.string   "title"
     t.datetime "posttime"
-    t.text     "body",       :limit => 255
+    t.text     "body",       limit: 255
     t.string   "cover"
     t.integer  "admin_id"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.decimal  "price"
+    t.string   "url"
   end
 
-  add_index "books", ["admin_id"], :name => "index_books_on_admin_id"
+  add_index "books", ["admin_id"], name: "index_books_on_admin_id"
 
-  create_table "carts", :force => true do |t|
+  create_table "carts", force: true do |t|
     t.string   "items"
     t.decimal  "cost"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "distributions", :force => true do |t|
+  create_table "distributions", force: true do |t|
     t.string   "title"
     t.datetime "posttime"
     t.text     "body"
     t.integer  "admin_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "distributions", ["admin_id"], :name => "index_distributions_on_admin_id"
+  add_index "distributions", ["admin_id"], name: "index_distributions_on_admin_id"
 
-  create_table "editors", :force => true do |t|
-    t.string   "name",       :default => "未命名",  :null => false
+  create_table "editors", force: true do |t|
+    t.string   "name",       default: "未命名",  null: false
     t.string   "portrait"
-    t.string   "level",      :default => "实习编辑", :null => false
+    t.string   "level",      default: "实习编辑", null: false
     t.text     "resume"
-    t.datetime "created_at",                     :null => false
-    t.datetime "updated_at",                     :null => false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
-  add_index "editors", ["name"], :name => "index_editors_on_name", :unique => true
+  add_index "editors", ["name"], name: "index_editors_on_name", unique: true
 
-  create_table "news", :force => true do |t|
+  create_table "news", force: true do |t|
     t.string   "title"
     t.datetime "posttime"
     t.text     "body"
     t.integer  "admin_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "news", ["admin_id"], :name => "index_news_on_admin_id"
+  add_index "news", ["admin_id"], name: "index_news_on_admin_id"
 
-  create_table "notices", :force => true do |t|
+  create_table "notices", force: true do |t|
     t.string   "title"
     t.datetime "posttime"
     t.text     "body"
     t.integer  "admin_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "notices", ["admin_id"], :name => "index_notices_on_admin_id"
+  add_index "notices", ["admin_id"], name: "index_notices_on_admin_id"
 
-  create_table "orders", :force => true do |t|
+  create_table "orders", force: true do |t|
     t.string   "items"
     t.decimal  "cost"
     t.datetime "ordertime"
     t.integer  "status"
     t.string   "notice"
     t.string   "deliverid"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "posts", :force => true do |t|
-    t.string   "title",      :default => "无题"
+  create_table "posts", force: true do |t|
+    t.string   "title",      default: "无题"
     t.datetime "posttime"
     t.text     "body"
-    t.integer  "editor_id",  :default => 0
-    t.datetime "created_at",                   :null => false
-    t.datetime "updated_at",                   :null => false
+    t.integer  "editor_id",  default: 0
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
-  add_index "posts", ["editor_id"], :name => "index_posts_on_editor_id"
-  add_index "posts", ["title"], :name => "index_posts_on_title"
+  add_index "posts", ["editor_id"], name: "index_posts_on_editor_id"
+  add_index "posts", ["title"], name: "index_posts_on_title"
 
 end
